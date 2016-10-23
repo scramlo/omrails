a7484f36e14909d69637bd8f9c1b434d9178f60a
Brian Scramlin <scramlo@hotmail.com>
2016-10-23 01:10:13 -0400

    Styled Registration Forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  as :user do
+    get "login" => "devise/sessions#new"
+    delete "logout" => "devise/sessions#destroy"
+    get "signup" => "devise/registrations#new"
+    get "account" => "devise/registrations#edit"
+  end
+
   root 'pages#home'
+
   get "about" => "pages#about"
   get "contact" => "pages#contact"
 end
