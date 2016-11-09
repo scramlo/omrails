5aea00e5e7cedea3b11a9a9beeff29517685d925
Brian Scramlin <scramlo@hotmail.com>
2016-11-09 15:31:37 -0500

    Tweaked following views. Still having problem with unfollowing needing to restart server to view prayers on home

--- app/models/prayer.rb
+++ app/models/prayer.rb
@@ -1,4 +1,5 @@
 class Prayer < ApplicationRecord
+  validates :body, length: { maximum: 140, message: "Oops! Please keep prayer requests under 140 characters!" }
   belongs_to :user
   has_many :comments
 end
